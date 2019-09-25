e8440a0b438f47e57db94b55a316c22f2ab2726e
Gustavo Zancheta <gustavo.ribeiro@triersistemas.com.br>
2019-09-25 13:30:08 -0300

    Commit inicialização Back-End

---
+++ GraphNetDev/src/main/java/br/com/GraphNet/GraphNetApplication.java
@@ -0,0 +1,13 @@
+package br.com.GraphNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphNetApplication.class, args);
+	}
+
+}
