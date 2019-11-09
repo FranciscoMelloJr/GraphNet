051cb98e5e2a75716e384675d7f7b4c42b9d8943
Oyuumi <classandorder@gmail.com>
2019-11-09 19:47:26 -0300

    ajustando os cadastros de teste

--- Back-end/src/main/java/br/unisul/services/DbService.java
+++ Back-end/src/main/java/br/unisul/services/DbService.java
@@ -35,7 +35,7 @@ public class DbService {
 		
 		Date data = new Date();
 
-		Cliente c1 = new Cliente(null, "Lucas", "lucas@gmail.com", "(48) 12345-6789", "88780-000", "123456789101", "", "");
+		Cliente c1 = new Cliente(null, "Lucas", "lucas@gmail.com", "(48) 12345-6789", "88780-000", "123456789101", "-49.5112", "-28.9395");
 
 		Provedor p1 = new Provedor(null, "Sul Internet", "Sul Telecom SA", "123456789", "88780-000",
 				"(48) 12345-6789", "sul@internet.com", "senha");
@@ -45,7 +45,7 @@ public class DbService {
 		
 		Solicitacao s1 = new Solicitacao(null, "");
 		
-		Caixa ca1 = new Caixa(null, "Caixa 1", "20", "20");
+		Caixa ca1 = new Caixa(null, "Caixa 1", "-49.51875310058597", "-28.933190306812516");
 		
 		s1.setCliente(c1);
 		s1.setProvedor(p1);
